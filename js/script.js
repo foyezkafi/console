e0d7c3a3fa60151f060f410fe5cef91275af1e17
foyezkafi <foyezkafi@gmail.com>
2025-11-02 12:48:33 +0600

    change

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@
 // }
 
 
-const myButton = document.querySelector('#clikbutton')
+const myButton = document.getElementById('#clikbutton')
 
-
-myButton.addEventListener('click',()=>{
-    console.log('kemon asen apni?')
-})
+    console.log('myButton')
+// myButton.addEventListener('click',()=>{
+//     console.log('kemon asen apni?')
+// })
 
 
 
