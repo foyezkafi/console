2407bd745b1aef84c1c39d36efe4dbb4eab3da95
foyezkafi <foyezkafi@gmail.com>
2025-11-02 11:32:50 +0600

    change

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// const age = prompt('Apner boyos koto?')
+// const income = prompt('apner sellary koto hole apni ay job niben?')
+
+
+// if(age >=20 && age <=50){
+//     if(income <=25000){
+//         console.log('ok job e apnake newa holo')
+//     }
+//     else{
+//         console.log('apni onk besi chaitechen')
+//     }
+    
+// }
+// else{
+//     console.log('apner boyos ta aro besi hole vhalo hoto')
+// }
+
+
+
