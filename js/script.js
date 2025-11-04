357df8c8f39337e2a62cccf0fd17edc9739dc15a
foyezkafi <foyezkafi@gmail.com>
2025-11-04 22:19:08 +0600

    change

--- js/script.js
+++ js/script.js
@@ -23,5 +23,50 @@
 //     console.log('kemon asen apni?')
 // })
 
+// -------for loop-----------
+
+// for ( let i=1; i<=5; i++){
+//     console.log("amar nam foyez kafi:", i);
+
+// }
+
+
+// -----------while loop------
+// let i = 1;
+// while (i <= 5) {
+//   console.log("Count:", i);
+//   i++;
+// }
+
+// --------------do while loop---------------
+
+// let j = 1;
+// do {
+//   console.log("Step:", j);
+//   j++;
+// } while (j <= 5);
+
+
+
+
+let gpa = prompt('apner ssc te GPA koto?')
+let mark = prompt('apner g-math and h-math e total mark koto')
+
+if(gpa ==5){
+    console.log('ok thik ase')
+    if(mark >=170){
+        console.log('okh')
+    }
+    else('mark ta 170 er besi hole newa jeto')
+}
+else{
+    console.log('apner gpa 5 hole newa jeto')
+}
+
+
+
+
+
+
 
 
