f02b716b04972956315e1d6abf9a3b65e25af9f5
foyezkafi <foyezkafi@gmail.com>
2025-11-07 20:55:52 +0600

    cane

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-const age = prompt('Apner boyos koto?')
-const income = prompt('apner sellary koto hole apni ay job niben?')
+// const age = prompt('Apner boyos koto?')
+// const income = prompt('apner sellary koto hole apni ay job niben?')
 
 
-if(age >=20 && age <=50){
-    if(income <=25000){
-        console.log('ok job e apnake newa holo')
-    }
-    else{
-        console.log('apni onk besi chaitechen')
-    }
+// if(age >=20 && age <=50){
+//     if(income <=25000){
+//         console.log('ok job e apnake newa holo')
+//     }
+//     else{
+//         console.log('apni onk besi chaitechen')
+//     }
     
-}
-else{
-    console.log('apner boyos ta aro besi hole vhalo hoto')
-}
+// }
+// else{
+//     console.log('apner boyos ta aro besi hole vhalo hoto')
+// }
 
 
 // const myButton = document.getElementById('#clikbutton')
@@ -65,8 +65,28 @@ else{
 
 
 
+// const alu = (alu , pera2 , country)=>{
+    
+
+//     console.log(alu)
+//     console.log(pera2)
+//     console.log(`i am living in ${country}`)
+
+// }
+
+// alu('lalalallalala' , 'tumi k?' , 'bangladesh')
 
 
 
 
+// const alu = (a , b)=>{
+    
+
+//     console.log(a+b)
+ 
+   
+
+// }
+
+// alu(10 , 30)
 
