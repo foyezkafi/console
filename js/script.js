288952a7682f278e0b256af0fd7d18a15134bb11
foyezkafi <foyezkafi@gmail.com>
2025-11-13 17:48:56 +0600

    rkure

--- js/script.js
+++ js/script.js
@@ -91,16 +91,16 @@
 // alu(10 , 30)
 
 
-// const button = document.querySelector('button')
-// const h1 = document.querySelector('h1')
+const button = document.querySelector('button')
+const h1 = document.querySelector('h1')
 
 
 
 
-// button.addEventListener('click' , ()=>{
-//     console.log('click hoise')
-//     h1.style ='color:red;'
-// })
+button.addEventListener('click' , ()=>{
+    console.log('click hoise')
+    h1.style ='color:red;'
+})
 
 
 
